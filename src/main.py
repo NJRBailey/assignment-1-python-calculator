55f1d597cad65d6b9a1adf2a07cb6c062925f329
Nicholas J R Bailey <njrbailey@outlook.com>
2024-12-19 21:55:32 +0000

    Show random inspirational quote

--- src/main.py
+++ src/main.py
@@ -1,7 +1,19 @@
 from datetime import datetime
+from random import randint
 
 now = datetime.now()
 print("It is currently " + str(now.hour) + ":" + str(now.minute) + " on " + str(now.day) + "/" + str(now.month) + "/" + str(now.year) + ".")
+
+messages = [
+  '"If I cannot do great things, I can do small things in a great way." —Martin Luther King, Jr.',
+  '"Act as if what you do makes a difference. It does." —William James',
+  '"Be the change that you wish to see in the world." —Mahatma Ghandi',
+  '"Don\'t sit down and wait for the opportunities to come. Get up and make them." —Madam C.J. Walker',
+  '"Opportunity is missed by most people because it is dressed in overalls and looks like work." —Thomas Edison'
+]
+message_index = randint(0, len(messages) - 1)
+print(messages[message_index])
+
 first_number = None
 while first_number == None:
   try:
