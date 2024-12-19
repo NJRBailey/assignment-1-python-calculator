665deb10d3cb06e89edad03f39655821c982288f
Nicholas J R Bailey <njrbailey@outlook.com>
2024-12-19 21:42:21 +0000

    Validate input

--- src/main.py
+++ src/main.py
@@ -1,6 +1,22 @@
-first_number = float(input("Enter the first number: "))
-operator = input("Enter the operator: ")
-second_number = float(input("Enter the second number: "))
+first_number = None
+while first_number == None:
+  try:
+    first_number = float(input("Enter the first number: "))
+  except ValueError:
+    print("Invalid input. Please enter a number.")
+operator = None
+while operator == None:
+  user_input = input("Enter the operator: ")
+  if user_input in ["+", "-", "*", "/", "^", ]:
+    operator = user_input
+  else:
+    print("Invalid input. Please enter a valid operator (+ - * / ^).")
+second_number = None
+while second_number == None:
+  try:
+    second_number = float(input("Enter the second number: "))
+  except ValueError:
+    print("Invalid input. Please enter a number.")
 if operator == "+":
   print(first_number + second_number)
 elif operator == "-":
