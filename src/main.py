fd661d0b86d109664d3ef5f273801f862ab63b84
Nicholas Bailey <njrbailey@outlook.com>
2024-12-22 09:27:32 +0000

    Allow the user to perform multiple calculations

--- src/main.py
+++ src/main.py
@@ -14,32 +14,34 @@ messages = [
 message_index = randint(0, len(messages) - 1)
 print(messages[message_index])
 
-first_number = None
-while first_number == None:
-  try:
-    first_number = float(input("Enter the first number: "))
-  except ValueError:
-    print("Invalid input. Please enter a number.")
-operator = None
-while operator == None:
-  user_input = input("Enter the operator: ")
-  if user_input in ["+", "-", "*", "/", "^", ]:
-    operator = user_input
-  else:
-    print("Invalid input. Please enter a valid operator (+ - * / ^).")
-second_number = None
-while second_number == None:
-  try:
-    second_number = float(input("Enter the second number: "))
-  except ValueError:
-    print("Invalid input. Please enter a number.")
-if operator == "+":
-  print(first_number + second_number)
-elif operator == "-":
-  print(first_number - second_number)
-elif operator == "*":
-  print(first_number * second_number)
-elif operator == "/":
-  print(first_number / second_number)
-elif operator == "^":
-  print(first_number ** second_number)
+run = True
+while run:
+  first_number = None
+  while first_number == None:
+    try:
+      first_number = float(input("Enter the first number: "))
+    except ValueError:
+      print("Invalid input. Please enter a number.")
+  operator = None
+  while operator == None:
+    user_input = input("Enter the operator: ")
+    if user_input in ["+", "-", "*", "/", "^", ]:
+      operator = user_input
+    else:
+      print("Invalid input. Please enter a valid operator (+ - * / ^).")
+  second_number = None
+  while second_number == None:
+    try:
+      second_number = float(input("Enter the second number: "))
+    except ValueError:
+      print("Invalid input. Please enter a number.")
+  if operator == "+":
+    print(first_number + second_number)
+  elif operator == "-":
+    print(first_number - second_number)
+  elif operator == "*":
+    print(first_number * second_number)
+  elif operator == "/":
+    print(first_number / second_number)
+  elif operator == "^":
+    print(first_number ** second_number)
